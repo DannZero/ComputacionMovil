bac9a88b25a8c5888bd9fbb784b02afd42db9a6f
Carlos Daniel Hernandez Chauteco <gamercarlos1530@gmail.com>
2019-03-11 11:31:41 -0600

    colorMix

--- ColorMix/ColorMix/ViewController.swift
+++ ColorMix/ColorMix/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBAction func switchAction(_ sender: Any) {
+    }
+    @IBAction func sliderAction(_ sender: Any) {
+    }
+    @IBAction func reset(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("ya me cargué")
+        colorView.layer.borderWidth = 5
+        colorView.layer.cornerRadius = 20
+        colorView.layer.borderColor = UIColor.black.cgColor
+        
     }
 
 
