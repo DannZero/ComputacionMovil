5540a488f463617a1b266e2ccbc2e609d1ce75cb
Carlos Daniel Hernandez Chauteco <gamercarlos1530@gmail.com>
2019-03-20 11:05:53 -0600

    piedra papel o tijeras

---
+++ PPT/PPT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PPT
+//
+//  Created by macbook on 3/20/19.
+//  Copyright © 2019 ioslab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
